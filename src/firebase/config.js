7826489df5d2bc3e22990b7533f641199234ad22
Yuvrajshankar <122772083+Yuvrajshankar@users.noreply.github.com>
2024-06-23 10:19:33 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBI8fIQVcDHyAADEzA0uZODnJXtP-cmj40",
-    authDomain: "todos-6aaee.firebaseapp.com",
-    projectId: "todos-6aaee",
-    storageBucket: "todos-6aaee.appspot.com",
-    messagingSenderId: "880189881968",
-    appId: "1:880189881968:web:b3e547ca3b091ccc0f7eb5",
-    measurementId: "G-KR3WYTQ6TP"
+    // ADD YOUR FIREBASE CONFIGURATION
 };
 
 const app = initializeApp(firebaseConfig);
